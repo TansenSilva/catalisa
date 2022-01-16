f9c7ee75683c42d701ee62ca77c9874ef8801da5
Tansen Silva <tansensilva@gmail.com>
2022-01-16 01:08:09 -0300

    Corrigir aquivo html e arquivo java

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const botao = document;querySelector('button');
+const botao = document.querySelector('button');
 const imagem1 = document.querySelector('#imagem1');
-const nomeDoPersonagem = document.querySelector('#nome1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
 const especie1 = document.querySelector('#especie1');
 const condicao1 = document.querySelector('#status1');
-const imagem1 = document.querySelector('#imagem1');
-const nomeDoPersonagem = document.querySelector('#nome1');
-const especie1 = document.querySelector('#especie1');
-const condicao1 = document.querySelector('#status1');
-const imagem1 = document.querySelector('#imagem1');
-const nomeDoPersonagem = document.querySelector('#nome1');
-const especie1 = document.querySelector('#especie1');
-const condicao1 = document.querySelector('#status1');
+const imagem2 = document.querySelector('#imagem2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+const imagem3 = document.querySelector('#imagem3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
 
+traduzirCondicao = (data) => {
+    if (data.status == 'unknown') {
+        return 'Não sabemos';
+    } else if (data.status == 'Alive') {
+        return 'Sim';
+    } else {
+        return 'Não. Está morto';
+    }
+}
+
+gerarValorAletorio = () => {
+    return Math.floor(Math.random() * 671);
+}
 
 pegarPersonagem1 = () => {
     let numeroAleatorio = gerarValorAletorio();
@@ -30,4 +43,46 @@ pegarPersonagem1 = () => {
     });
 }
 
-botao.onclick = pegarPersonagem1;
+pegarPersonagem2 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = traduzirCondicao(data);
+    });
+}
+
+pegarPersonagem3 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem3.src = data.image;
+        imagem3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = traduzirCondicao(data);
+    });
+}
+
+executarBusca = () => {
+    pegarPersonagem1();
+    pegarPersonagem2();
+    pegarPersonagem3();
+    const valor = gerarValorAletorio()
+    console.log(valor)
+}
+
+botao.onclick = executarBusca;
