4d4f71a52e9feeb98a5c59741f72f3f84681c383
Tansen Silva <tansensilva@gmail.com>
2022-01-16 00:51:12 -0300

    Criar conexão com API Rick and morty

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const botao = document;querySelector('button');
-const imagem1 = document.querySelector('Imagem');
-const nomeDoPersonagem = document.querySelector('nome');
-const especie1 = document.querySelector('especie');
-const condicao1 = document.querySelector('status');
+const imagem1 = document.querySelector('#imagem1');
+const nomeDoPersonagem = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+const imagem1 = document.querySelector('#imagem1');
+const nomeDoPersonagem = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+const imagem1 = document.querySelector('#imagem1');
+const nomeDoPersonagem = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+
+
+pegarPersonagem1 = () => {
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem1.src = data.image;
+        imagem1.alt = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML = traduzirCondicao(data);
+    });
+}
+
+botao.onclick = pegarPersonagem1;
