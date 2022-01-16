43a86a262663856c28a4d6da66750479b1fefca3
Tansen Silva <tansensilva@gmail.com>
2022-01-16 00:44:15 -0300

    Criar constantes no arquivo script

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document;querySelector('button');
+const imagem1 = document.querySelector('Imagem');
+const nomeDoPersonagem = document.querySelector('nome');
+const especie1 = document.querySelector('especie');
+const condicao1 = document.querySelector('status');
